c6bee2a2e52db99a0c49a345b8d7657e9944b833
Nguyen Van Trien <99782622+toroieno@users.noreply.github.com>
2022-12-17 02:42:07 +0700

    st

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
-import { i18n } from "./i18n";
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-  i18n,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App)
+}).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ],
-
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false,
-      enableBridge: false
-    }
-  }
+  ]
 })
