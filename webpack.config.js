20097c58b05e7221e1fc97b8fc0ccd3b941c0aa8
NVTuann201 <nvtuann201@gmail.com>
2022-12-17 02:16:40 +0700

    date time

--- webpack.config.js
+++ webpack.config.js
@@ -6,25 +6,21 @@ module.exports = {
       {
         test: /\.s(c|a)ss$/,
         use: [
-          'vue-style-loader',
-          'css-loader',
+          "vue-style-loader",
+          "css-loader",
           {
-            loader: 'sass-loader',
+            loader: "sass-loader",
             // Requires sass-loader@^7.0.0
             options: {
-              implementation: require('sass'),
-              indentedSyntax: true // optional
-            },
-            // Requires >= sass-loader@^8.0.0
-            options: {
-              implementation: require('sass'),
+              implementation: require("sass"),
+              indentedSyntax: true, // optional
               sassOptions: {
-                indentedSyntax: true // optional
+                indentedSyntax: true, // optional
               },
             },
           },
         ],
       },
     ],
-  }
-}
+  },
+};
